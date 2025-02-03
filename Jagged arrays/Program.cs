196e4e79214a447642d33906670a632b9f4cb550
Muhammed <Muhammed@DESKTOP-DT4KHUN>
2025-02-03 13:53:36 +0400

    Add project files.

---
+++ Jagged arrays/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("How many rows?");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Which symbol?");
+string symbol = Console.ReadLine();
+
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < rows - i - 1; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int j = 0; j < 2 * i + 1; j++)
+    {
+        Console.Write(symbol);
+    }
+
+    Console.WriteLine();
+}
+
+
+for (int i = rows - 2  ; i >= 0 ; i--)
+{
+    for (int j = 0; j < rows - i - 1; j++)
+    {
+        Console.Write(" ");
+
+    }
+
+    for (int j = 0; j <  2 * i + 1; j++)
+    {
+        Console.Write(symbol);
+    }
+
+    Console.WriteLine();   
+}
